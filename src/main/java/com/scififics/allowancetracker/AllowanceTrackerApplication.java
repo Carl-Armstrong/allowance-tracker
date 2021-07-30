6d6069772aa32270ea4964e55165fd2f05056c2c
Carl <carljarms@gmail.com>
2021-07-29 19:04:46 -0500

    Initial commit

---
+++ src/main/java/com/scififics/allowancetracker/AllowanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.scififics.allowancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllowanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllowanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scififics/allowancetracker/AllowanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scififics.allowancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllowanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
